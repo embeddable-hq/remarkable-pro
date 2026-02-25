744953d562047dff1b9ea157f9d9576a6f3776c3
mad-raccoon <ribeiro.ruifigueiredo@gmail.com>
2026-02-25 15:51:57 +0000

    test config

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    include: ['**/*.test.ts', '**/*.test.tsx'],
+    include: ['src/**/*.test.{ts,tsx}'],
     environment: 'jsdom',
     globals: true,
     setupFiles: ['./vitest.setup.ts'],
