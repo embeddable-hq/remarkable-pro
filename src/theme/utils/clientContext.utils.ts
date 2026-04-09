fa2875b26507f8b062b355d16fa3b3cd1dd45685
mad-raccoon <ribeiro.ruifigueiredo@gmail.com>
2026-04-09 15:48:38 +0200

    improve timezone validation

--- src/theme/utils/clientContext.utils.ts
+++ src/theme/utils/clientContext.utils.ts
@@ -1,5 +1,10 @@
 export const getClientContextTimezone = (tz?: string): string | undefined => {
   if (!tz) return undefined;
+
+  if (typeof Intl.supportedValuesOf === 'function') {
+    return Intl.supportedValuesOf('timeZone').includes(tz) ? tz : undefined;
+  }
+
   try {
     Intl.DateTimeFormat(undefined, { timeZone: tz });
     return tz;
