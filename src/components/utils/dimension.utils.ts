ea195e8d3378292b1788c124a5eb30ed3e9338af
mad-raccoon <ribeiro.ruifigueiredo@gmail.com>
2026-04-28 10:39:55 +0100

    fix week ranges

--- src/components/utils/dimension.utils.ts
+++ src/components/utils/dimension.utils.ts
@@ -1,8 +1,10 @@
 import { CUBE_DIMENSION_TYPE_TIME, Dimension, Granularity, TimeRange } from '@embeddable.com/core';
-import dayjs from 'dayjs';
+import dayjs, { QUnitType } from 'dayjs';
+import isoWeek from 'dayjs/plugin/isoWeek.js';
 import utc from 'dayjs/plugin/utc.js';
 
 dayjs.extend(utc);
+dayjs.extend(isoWeek);
 
 /**
  * Constructs a TimeRange covering the full granularity bucket for a clicked time-axis bar.
@@ -29,10 +31,11 @@ export const getTimeRangeFromDimensionValue = ({
 
   const granularity = stateGranularity ?? dimension?.inputs?.granularity ?? 'day';
 
-  const d = dayjs.utc(value);
+  const isoGranularity = granularity === 'week' ? 'isoWeek' : granularity;
+  const utcDate = dayjs.utc(value);
   return {
-    from: d.startOf(granularity).toDate(),
-    to: d.endOf(granularity).toDate(),
+    from: utcDate.startOf(isoGranularity as QUnitType).toDate(),
+    to: utcDate.endOf(isoGranularity as QUnitType).toDate(),
     relativeTimeString: undefined,
   };
 };
